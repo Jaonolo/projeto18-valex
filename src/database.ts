ebdc1cb119a6bff1ff3afd140461689bb26b5f0d
João Vitor Cezario Sanches <joaovitormanolo@gmail.com>
2022-07-12 16:48:56 -0300

    fix : last minutes fixes

--- src/database.ts
+++ src/database.ts
@@ -3,6 +3,19 @@ import pg from "pg";
 dotenv.config();
 
 const { Pool } = pg;
-export const connection = new Pool({
+
+const configDatabase : {
+  connectionString: string | undefined,
+  ssl: any
+} = {
   connectionString: process.env.DATABASE_URL,
-});
+  ssl: undefined
+}
+
+if(process.env.MODE === "PROD") {
+  configDatabase.ssl = {
+    rejectUnauthorized: false
+  }
+}
+
+export const connection = new Pool(configDatabase);
